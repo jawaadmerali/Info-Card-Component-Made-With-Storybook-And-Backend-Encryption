ab3bf5520e6f3fffa774e64a3028c98fd6b635a7
jawaadmerali <113218507+jawaadmerali@users.noreply.github.com>
2023-08-10 22:39:23 -0500

    Added info-card.presentational.vue
    This encompasses the presentational file

---
+++ info-card.presentational.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="info-card-presentational large inline flex vertical bg-default corner outline text-center left">
+        <i :class="`f1 circle large default disabled ${ iconClass }`"></i>
+        <div class="f4 text-bold">{{ headerText }}</div>
+        <p>{{ descriptionText }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InfoCardPresentational',
+    props: {
+      iconClass: String,
+      headerText: String,
+      descriptionText: String
+    },
+};
+</script>
